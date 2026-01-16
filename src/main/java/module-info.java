ca53a86653f6dcbebd3d9530e5d871d556b39ad7
Ancharca <A.l.e.n.a.D.VRN@gmail.com>
2026-01-16 18:12:24 +0300

    Delete src/main/java/module-info.java
    для тестов не нужен

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.mathpartofrender {
-    requires java.desktop;
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.junit.jupiter.api;
-    requires org.junit.jupiter.engine;
-    requires org.junit.platform.commons;
-}
